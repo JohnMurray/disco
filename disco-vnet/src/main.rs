f46b3b17a5f72b47ca8de27521f553ae6dc92cf0
John Murray <me@johnmurray.io>
2020-04-05 11:37:01 -0400

    notes / do-next

--- disco-vnet/src/main.rs
+++ disco-vnet/src/main.rs
@@ -1,3 +1,14 @@
+/// # virtual-interface experiment
+/// 
+/// The goal here is to create a super simple virtual interface that can intercept
+/// all local network traffic (perhaps with some help from iptables and friends)
+///
+/// ## do next
+/// 
+///   [ ] create a tun/tap interface and successfully init the sockets and such
+///   [ ] try to read some packets sent to the assigned IP address
+///   [ ] try to reach the IP from some other node on the network
+
 fn main() {
     println!("Hello, world!");
 }
